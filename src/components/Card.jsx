997dc1098afae5fafc132e24bec1192c2494eba0
santiruartes <ruartes19@gmail.com>
2024-01-25 20:43:09 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+
+export const Card = () => {
+
+
+  return (
+    <div className="bg-black px-10 py-8 rounded-md">
+      <div className="text-2x1 font-bold text-black rounded-md p-10 bg-white ">Prueba con card</div>
+    </div>
+  )
+}
---
+++ src/components/SwitchTheme.jsx
@@ -0,0 +1,9 @@
+
+
+export const SwitchTheme = () => {
+    
+
+    return (
+    <div>SwitchTheme</div>
+  )
+}
